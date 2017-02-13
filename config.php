2dfc94e3feb736c3647e0841070047518a7cd87f
camilapaulino <camilla_rebeca@hotmail.com>
2017-02-13 21:34:15 -0200

    alter config

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 <?php  
 
 define('servidor', 'localhost', true);
-define('banco', 'visualjob', true);
+define('banco', 'free', true);
 define('usuario', 'root', true);
 define('senha', '', true);
 
