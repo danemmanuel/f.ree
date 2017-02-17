3d95d7fea6bec77fa81d66cd2cd4cbf586f1abbf
danemmanuel <danemmanuelturibio@gmail.com>
2017-02-17 10:30:39 -0200

    update

--- conta/includes/freelancer.php
+++ conta/includes/freelancer.php
@@ -1,18 +1,20 @@
 <?php 
-            require_once '../php/class/freelancer.class.php';
+require_once '../php/class/freelancer.class.php';
 
-            $freelancer=new freelancer();
-            $idfreelancer=$_SESSION['idfreelancer'];
-            $freelancer->setId($idfreelancer);
-            $result = $freelancer->buscarId();
+$freelancer=new freelancer();
+$idfreelancer=$_SESSION['idfreelancer'];
+$freelancer->setId($idfreelancer);
+$result = $freelancer->buscarId();
 
-            $nomefreelancer=$result['nome'];
-            $email=$result['email'];
-            $telefone=$result['telefone'];
-            $datanascimento=$result['datanascimento'];
-            $sexo=$result['sexo'];
+$nomefreelancer=$result['nome'];
+$email=$result['email'];
+$telefone=$result['telefone'];
+$datanascimento=$result['datanascimento'];
+$sexo=$result['sexo'];
+$senha=$result['senha'];
 
-            $datanova = date("d-m-Y", strtotime($datanascimento));
+
+$datanova = date("d-m-Y", strtotime($datanascimento));
 
 
-            ?>
+?>
--- conta/index.php
+++ conta/index.php
@@ -2,29 +2,29 @@
 session_start();
 if(isset($_SESSION['idfreelancer'])){
 
-require_once 'includes/freelancer.php';
+  require_once 'includes/freelancer.php';
 
- $menu=file_get_contents(realpath(dirname(__FILE__) . '/includes/menu.php'));
+  $menu=file_get_contents(realpath(dirname(__FILE__) . '/includes/menu.php'));
 
- ?>         
+  ?>         
 
- <!DOCTYPE html>
- <html>
- <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>F.ree > Minha Conta</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <!-- Bootstrap 3.3.6 -->
-  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="plugins/datepicker/datepicker3.css">
-  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>F.ree > Minha Conta</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.6 -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="plugins/datepicker/datepicker3.css">
+    <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
   folder instead of downloading all of them to reduce the load. -->
   <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
